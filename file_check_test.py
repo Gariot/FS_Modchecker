cc41c62951604c312c44c33de7bbcd1213a2d26a
Gariot <mathias_depp@yahoo.de>
2020-04-05 22:44:11 +0200

    Modcheck nochmal ueberarbeitet, danke Ketzu :D

--- file_check_test.py
+++ file_check_test.py
@@ -6,12 +6,11 @@ import glob
 import xml.etree.ElementTree as ET
 import file_paths as mod_paths
 
-
 modFiles = [f for f in glob.glob(mod_paths.modOrdnerPath + "**/*.zip", recursive=True)]  # Mit einer List comprehension mithilfe von glob alle Dateien parsen
 modNames = []
 
-for e in modFiles:
-    modNames.append(e.split('\\')[7][:-4])
+#for e in modFiles:
+#    modNames.append(e.split('\\')[7][:-4])  # Die ModFiles liste splitten und die Strings so slicen das nur der modname ueber bleibt
 
 # Hier werden die Aktiven Mods bestimmt
 saveGame = ET.parse(mod_paths.saveGamePath)  # XML des Savegames parsen
@@ -19,16 +18,24 @@ root = saveGame.getroot()  # Die "wurzel" abspeichern
 activeMods = []  # Leere Liste fuer die aktiven mods generieren
 
 for mod in root.iter('mod'):  # Mit einer Schleife ueber die Elemente der XML gehen die "mod" im tag haben
-    # print(mod.get('modName'))
     activeMods.append(mod.get('modName'))  # den Modnamen in die Liste einfuegen
 
 # Test ob ein Element in einer anderen Liste ist
-for mod in modNames:
-    if mod in activeMods:
-        print("Die Mod: " + mod + " gehoert in den Spielstand.")
-    else:
-        print("Die Mod: " + mod + " gehoert NICHT in den Spielstand.")
+#for mod in modNames:
+#    if mod in activeMods:
+#        print("Die Mod: " + mod + " gehoert in den Spielstand/Modordner.")
+#    else:
+#        print("Die Mod: " + mod + " gehoert NICHT in den Spielstand/Modordner.")
 
+for mod in modFiles:
+    if any([y in mod for y in activeMods]):
+        print("Die Mod: " + mod + " ist aktiv.")
+    else:
+        print("Die Mod: " + mod + " ist nicht aktiv.")
+
+# Stats
 print("")
 print("Mods insgesamt im Ordner: " + str(len(modNames)))
 print("Mods insgesamt im Spielstand aktiviert: " + str(len(activeMods)))
+print("")
+
