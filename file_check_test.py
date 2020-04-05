6ccc1007be2e477d3765dbc0002959c69b12375b
Gariot <mathias_depp@yahoo.de>
2020-04-05 22:49:06 +0200

    Cleanup

--- file_check_test.py
+++ file_check_test.py
@@ -6,11 +6,13 @@ import glob
 import xml.etree.ElementTree as ET
 import file_paths as mod_paths
 
-modFiles = [f for f in glob.glob(mod_paths.modOrdnerPath + "**/*.zip", recursive=True)]  # Mit einer List comprehension mithilfe von glob alle Dateien parsen
+modFiles = [f for f in glob.glob(mod_paths.modOrdnerPath + "**/*.zip", recursive=True)]  # Mithilfe von glob alle
+#                                                                                          Dateien parsen
 modNames = []
 
-#for e in modFiles:
-#    modNames.append(e.split('\\')[7][:-4])  # Die ModFiles liste splitten und die Strings so slicen das nur der modname ueber bleibt
+# for e in modFiles:
+#    modNames.append(e.split('\\')[7][:-4])  # Die ModFiles liste splitten und die Strings so slicen das nur der
+#                                              modname ueber bleibt
 
 # Hier werden die Aktiven Mods bestimmt
 saveGame = ET.parse(mod_paths.saveGamePath)  # XML des Savegames parsen
@@ -21,7 +23,7 @@ for mod in root.iter('mod'):  # Mit einer Schleife ueber die Elemente der XML ge
     activeMods.append(mod.get('modName'))  # den Modnamen in die Liste einfuegen
 
 # Test ob ein Element in einer anderen Liste ist
-#for mod in modNames:
+# for mod in modNames:
 #    if mod in activeMods:
 #        print("Die Mod: " + mod + " gehoert in den Spielstand/Modordner.")
 #    else:
